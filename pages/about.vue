b5ee485b98b4f0434379c6cb02a976bddc54947f
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2021-05-14 14:31:51 -0400

    abbr trials

--- pages/about.vue
+++ pages/about.vue
@@ -2,20 +2,24 @@
     <b-container fuild class="d-flex justify-content-center align-items-start p-5">
         <b-row>
             <b-col lg="12" class="p-5">
-                <h1 v-html="data.name"></h1>
+                <h1 v-html="data.banner.title"></h1>
+            </b-col>
+            <b-col lg="12" class="p-5">
+                <h2 v-html="data.section_one.title"></h2>
+                <p v-html="data.section_one.text"></p>
             </b-col>
         </b-row>
     </b-container>
 </template>
 
 <script>
-import Content from '@/content/content.json';
+import content from '../content/content.json';
 
 export default {
     name: 'about',
     data() {
         return {
-            data: Content
+            data: content.page_one
         }
     }
 }

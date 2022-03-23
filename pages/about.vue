4459a86419c6e47171c7d09deaff843dfc0f2398
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2022-03-22 20:54:00 -0400

    Update content model

--- pages/about.vue
+++ pages/about.vue
@@ -2,24 +2,24 @@
     <b-container fuild class="d-flex justify-content-center align-items-start p-5">
         <b-row>
             <b-col lg="12" class="p-5">
-                <h1 v-html="data.banner.title"></h1>
+                <h1 v-html="data.title"></h1>
             </b-col>
             <b-col lg="12" class="p-5">
-                <h2 v-html="data.section_one.title"></h2>
-                <p v-html="data.section_one.text"></p>
+                <!-- <h2 v-html="data.section_one.title"></h2> -->
+                <!-- <p v-html="data.section_one.text"></p> -->
             </b-col>
         </b-row>
     </b-container>
 </template>
 
 <script>
-import content from '../content/content.json';
+import content from '../content/frontaid.content.json';
 
 export default {
     name: 'about',
     data() {
         return {
-            data: content.page_one
+            data: content
         }
     }
 }

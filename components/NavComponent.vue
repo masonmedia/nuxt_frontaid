6431a1abba248d0ea76de947fcdaf268e4f3ecd3
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2022-03-22 22:14:57 -0400

    content update

--- components/NavComponent.vue
+++ components/NavComponent.vue
@@ -12,11 +12,14 @@
    
     <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
       <div class="px-3 py-2">
-        <p>
+        <router-link 
+        v-for="route in this.$router.options.routes" :key="route.path"
+        :to="route.path" v-html="route.name"></router-link>
+        <!-- <p>
           Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
           in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img> -->
       </div>
     </b-sidebar>
     </div>
--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,8 @@
 <template>
-    <b-container fuild class="d-flex justify-content-center align-items-start p-5">
-        <b-row>
-            <b-col lg="12" class="p-5">
+    <b-container fluid class="p-0">
+        <b-row align-v="center" class="min-vh-100 bg-light p-0 m-0">
+            <b-col align-self="center" lg="12" class="p-5">
                 <h1 v-html="data.title"></h1>
-            </b-col>
-            <b-col lg="12" class="p-5">
-                <!-- <h2 v-html="data.section_one.title"></h2> -->
-                <!-- <p v-html="data.section_one.text"></p> -->
             </b-col>
         </b-row>
     </b-container>
@@ -19,8 +15,12 @@ export default {
     name: 'about',
     data() {
         return {
-            data: content
+            data: content,
+            articles: ''
         }
-    }
+    },
+    async fetch() {
+      this.articles = await fetch("https://jsonplaceholder.typicode.com/posts").then((res) => res.json());
+    },
 }
 </script>

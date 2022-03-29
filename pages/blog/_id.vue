8846fd9d026a091eb0069e5220799cf1e1eab0c6
Natalie <natalie@Alines-MacBook-Pro-5.local>
2022-03-28 22:05:51 -0400

    Updates

---
+++ pages/blog/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="">
+        <b-container fluid>
+            <b-row align-v="center" class="min-vh-100">
+                <b-col lg="12">
+                    <h1>{{ post.title }}</h1>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    id: this.$route.params.id,
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        posts() {
+            this.posts.find(post => post.id == this.id)
+        }
+    }
+}
+</script>

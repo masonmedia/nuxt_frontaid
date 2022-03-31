38d7920d0d8b3b0591e0ac2207e985440159a260
masonmedia <masonmedia@users.noreply.github.com>
2022-03-31 18:50:19 +0000

    deploy: 75af1149acb96cac9ee831783e9fcc2e2beb538e

--- _nuxt/static/1648751432/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_frontaid\u002F_nuxt\u002Fstatic\u002F1648751432",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt_frontaid\u002F",assetsPath:"\u002Fnuxt_frontaid\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"86e2e93d"}}}}(null));
